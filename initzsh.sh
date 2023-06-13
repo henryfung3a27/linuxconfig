37204781d56bdeb34e5e83a90c701e92bdce5ca3
henryfung3a27 <henryfung3a27@yahoo.com.hk>
2023-06-13 10:20:36 +0800

    Update initzsh.sh

--- initzsh.sh
+++ initzsh.sh
@@ -1,7 +1,30 @@
-echo "Installing zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# Exit when any command fails
+set -e
+
+OMZ_URL="https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh"
+
+command_exists() {
+  command -v "$@" >/dev/null 2>&1
+}
+
+# Check zsh installed
+command_exists zsh || {
+        # Install with apt if available
+        command_exists apt || {
+                # If no apt, install zsh yourself
+                echo "Zsh is not installed. Install it first!"
+                exit 1
+        }
+        echo "Installing zsh"
+        sudo apt install zsh -y
+}
+
+echo "Installing Oh My Zsh..."
+# CHSH=Change default shell to zsh after installation
+# RUNZSH=Run zsh after installation. Set no to continue this script execution
+CHSH=yes RUNZSH=no sh -c "$(curl -fsSL "$OMZ_URL")"
+
 echo
 echo "Setting new theme..."
-sed -i "s/robbyrussel/tjkirch/g" ~/.zshrc
-source ~/.zshrc
-echo "Init success"
+sed -i "s/robbyrussell/tjkirch/g" ~/.zshrc
+echo "Init success. Run zsh now or restart the terminal."
